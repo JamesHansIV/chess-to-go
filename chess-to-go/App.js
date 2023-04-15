4d100b26d5e9bbfe47a36b43cf2d626fe48047d5
Elen Bhattarai <elen@Elens-MacBook-Air.local>
2023-04-15 04:07:03 -0500

    upload image button, stack navigator to another screen, take pictures, etc

--- chess-to-go/App.js
+++ chess-to-go/App.js
@@ -2,11 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button  } from 'react-native';
 import HomePage from './pages/HomePage';
+import GameLanding from './pages/GameLanding';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <HomePage/>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomePage} />
+      <Stack.Screen name="GameLanding" component={GameLanding} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
