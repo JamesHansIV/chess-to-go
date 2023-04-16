83cd2a2896eb4c01c37028063ff87be4445c845b
ElenBhattarai <elenbhattarai2002@ku.edu>
2023-04-16 04:25:14 -0500

    hotfix

--- backend/py/preprocess.py
+++ backend/py/preprocess.py
@@ -9,7 +9,7 @@ def performTransform(filename, corner_points, img_height, img_width):
     img = cv2.imread(filename)
     
     # corner_points_list = corner_points
-    orig_points_list = [[corner_points[0:2]],[corner_points[2:4]],[corner_points[4:6]],[corner_points[6:8]]]
+    orig_points_list = [corner_points[0:2],corner_points[2:4],corner_points[4:6], corner_points[6:8]]
     print("unflattendd", orig_points_list)
 
     # convert colors and contrast
